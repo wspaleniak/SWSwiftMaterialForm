5c4f197c40c709cfd359e1b4bba9711eeb46969a
wspaleniak <w.spaleniak@o2.pl>
2025-03-07 13:40:13 +0100

    Add compatibility with other platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SWSwiftMaterialForm",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
+        .watchOS(.v8),
+        .macOS(.v12),
+        .tvOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
